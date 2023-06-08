a4ef90b1bd7d9043ea6ce3d08af90cfe5a01390f
Thrinadh-Kumpatla <kthrinadh8898@gmail.com>
2023-06-08 19:19:24 +0530

    add logger and support through helm chart

--- src/print_timestamp.py
+++ src/print_timestamp.py
@@ -1,14 +1,32 @@
+import datetime
 from flask import Flask
 import time
+import logging
 from prometheus_flask_exporter import PrometheusMetrics
+from waitress import serve
+import os
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
+# Configure logging
+log_level = os.environ.get('LOG_LEVEL', 'error').upper()
+numeric_level = getattr(logging, log_level, logging.INFO)
+logging.basicConfig(level=numeric_level)
+logger = logging.getLogger(__name__)
+
 @app.route('/')
 def get_timestamp():
-    ts = time.time()
-    return f"{ts}"
+    try:
+        ts = time.time()
+        dt = datetime.datetime.fromtimestamp(ts)
+        month = dt.strftime("%B")
+        logger.info("Timestamp requested: %s", ts)
+        logger.debug("Debug message: current running month is %s", month)
+        return f"{ts}"
+    except Exception as e:
+        logger.error("An error occurred: %s", str(e))
+        return "Error occurred"
 
 @app.route('/health')
 @metrics.do_not_track()
@@ -16,4 +34,5 @@ def health_check():
     return "OK"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    logger.info("Starting Flask application...")
+    serve(app, host='0.0.0.0', port=5001)
